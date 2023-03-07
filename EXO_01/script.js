f537f34a73541488f97f08f73fd01eb91e1e77ed
Bazurogg <bazurogg@gmail.com>
2023-03-07 14:31:21 +0100

    Adding spacing

--- EXO_01/script.js
+++ EXO_01/script.js
@@ -4,19 +4,27 @@ box.classList.add("box")
 const board = document.querySelector("#board")
 board.appendChild(box)
 box.innerText = "HELLO !"
+
 box.addEventListener("click", function(){
+
     let className = box.getAttribute("class")
     let style = window.getComputedStyle(box)
     let classAttributes = ""
+
     for (let i = 0; i < style.length; i++){
+
         let attribute = style[i]
+        
         if (attribute === "background-color" || attribute === "color" || attribute === "height" || attribute === "width" || attribute === "display"){
 
             let attributeFormat = attribute.replace(/^[a-z]/g, function(match){
                 return match.toUpperCase() + match.slice(1)
             })
+
             classAttributes += "- " + attributeFormat + " : " + style.getPropertyValue(style[i]) + "\n"
         }
-    }    
+    }  
+
     alert("Class Name : " + className + "\n\n" + classAttributes + "- Font: " + style.getPropertyValue("font-family") + " (" + style.getPropertyValue("font-size") + ")")
+
 })
