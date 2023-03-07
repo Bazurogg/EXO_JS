406e4c25198e4a4609e3efb989e92688507fa57f
Bazurogg <bazurogg@gmail.com>
2023-03-07 13:20:21 +0100

    Add event "on click" on boxes
    function alternate between cross and circle ----> Working

--- EXO_MORPION/morpion.js
+++ EXO_MORPION/morpion.js
@@ -5,17 +5,36 @@ box.classList.add("box")
 const board = document.querySelector("#board")
  
 let nbBox = 9
+let symbol = 'X'
 
 for (let i = 1; i <= nbBox; i++){
     let newbox = box.cloneNode()
     board.appendChild(newbox)
     
     function addSymbol(box){
-        box.classList.add("box-circle")
-    }
+
+        if (box.textContent !== '') {
+            return
+        }
+
+        box.textContent = symbol
+
+        // 1 er Test avec un l'opérateur ternaire (! non cocluant:
+         
+        // symbol = (symbol === 'X') ? 'O' + box.classList.add("box-circle") : 'X' + box.classList.add("box-cross") 
+
+        if (symbol === 'X') {
+            box.classList.add("box-cross");
+            symbol = 'O';
+          } else {
+            box.classList.add("box-circle");
+            symbol = 'X';
+        }
+            
+    } 
+    
     
     newbox.addEventListener("click", function(){
         addSymbol(newbox)
-        newbox.textContent = 'O'
     })
 }
