0ce33367f0bbafde781887d1bfec7eb2c5f2efd2
Bazurogg <bazurogg@gmail.com>
2023-03-09 12:10:03 +0100

    Upgrading keydown creating and deleting boxes

--- EXO_04/script.js
+++ EXO_04/script.js
@@ -3,34 +3,41 @@
 const board = document.querySelector("#board")
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color
+}
 
 document.addEventListener('keydown', function(event){
-
-    if (event.key === "ArrowDown"){
-        event.preventDefault() // supprime l' event par défaut de la touche
-        const box = document.createElement("div")
-        box.classList.add("box")
-        box.style.backgroundColor = getRandomColor()
-        board.appendChild (box)
+  
+  if (event.key === "ArrowDown"){
+    
+    if (board.querySelectorAll(".box").length < 225){ // On va limiter le nombre de "box" contenues dans le "#board"
+      
+      event.preventDefault() // supprime l' event par défaut de la touche
+      const box = document.createElement("div")
+      box.classList.add("box")
+      box.style.backgroundColor = getRandomColor()
+      board.appendChild (box)
+      
     }
     
-    if (event.key === "ArrowUp"){
-        // event.preventDefault() // supprime l' event par défaut de la touche
-        const box = document.getElementById("box")
-        const board = document.querySelector("#board")
-        box.board.removeChild(box)
+  } else if (event.key === "ArrowUp"){
 
+    event.preventDefault() // supprime l' event par défaut de la touche
+    board.removeChild(board.lastElementChild)
+  
+    //Exemple récupérer sur W3School :
+    // function myFunction() {
+    //     const list = document.getElementById("myList");
+    //     list.removeChild(list.lastElementChild);
+    // }
+    
+  }
+  
 
-        // function myFunction() {
-        //     const list = document.getElementById("myList");
-        //     list.removeChild(list.lastElementChild);
-    }
-
+  
 })
