c46d7d0e8b967c27b1048e74f124edd5712e878c
Bazurogg <bazurogg@gmail.com>
2023-03-12 21:42:28 +0100

    Exo 05 Cleaning code
    exo 05 ---> OK finish !

--- EXO_05/script02.js
+++ EXO_05/script02.js
@@ -8,22 +8,10 @@ function convertirEnFrancs() {
     const eurosValue = eurosInput.value;
 
     if (isNaN(eurosValue)) {
-        alert('Merci de saisir un nombre. Conversion Impossible.');
+        alert('Incorrect value ! Please type a number. ');
         eurosInput.value = '1';
     } else {
         const francsValue = eurosValue * 6.55957;
         francsOutput.value = francsValue.toFixed(2);
-    }
-}
-
-function convertirEnEuros() {
-    const francsValue = francsOutput.value;
-
-    if (isNaN(francsValue)) {
-        alert('Merci de saisir un nombre. Conversion Impossible.');
-        francsOutput.value = eurosInput.value * 6.55957;
-    } else {
-        const eurosValue = francsValue / 6.55957;
-        eurosInput.value = eurosValue.toFixed(2);
     }
 }
